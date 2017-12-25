61a907a4b206fdb19570de265ce77486dc77a99f
kimu_shu <alfvegardrisc@gmail.com>
2017-12-25 16:20:25 +0900

    Change measure functions to inline

--- peridot_rpc_server/HAL/inc/bson.h
+++ peridot_rpc_server/HAL/inc/bson.h
@@ -19,25 +19,61 @@ extern int bson_get_double(const void *doc, int offset, double default_value);
 
 extern void bson_create_empty_document(void *doc);
 extern int bson_set_string(void *doc, const char *key, const char *string);
-extern int bson_measure_string(const char *key, const char *string);
 extern int bson_set_subdocument(void *doc, const char *key, const void *sub_doc);
-extern int bson_measure_subdocument(const char *key, const void *doc);
 extern int bson_set_array(void *doc, const char *key, const void *sub_doc);
-extern int bson_measure_array(const char *key, const void *doc);
 extern int bson_set_boolean(void *doc, const char *key, int value);
-extern int bson_measure_boolean(const char *key);
 extern int bson_set_int32(void *doc, const char *key, int value);
-extern int bson_measure_int32(const char *key);
 extern int bson_set_double(void *doc, const char *key, double value);
-extern int bson_measure_double(const char *key);
 extern int bson_set_null(void *doc, const char *key);
-extern int bson_measure_null(const char *key);
 extern int bson_set_binary_generic(void *doc, const char* key, int binlen, void **bufptr);
-extern int bson_measure_binary(const char *key, int binlen);
 extern int bson_shrink_binary(void *doc, void *buf, int binlen);
 extern int bson_set_element(void *doc1, const char *key, const void *doc2, int offset);
-extern int bson_measure_element(const char *key, const void *doc, int offset);
 extern int bson_measure_document(const void *doc);
+
+static inline int bson_measure_string(const char *key, const char *string)
+{
+    return bson_set_string(NULL, key, string);
+}
+
+static inline int bson_measure_subdocument(const char *key, const void *doc)
+{
+    return bson_set_subdocument(NULL, key, doc);
+}
+
+static inline int bson_measure_array(const char *key, const void *doc)
+{
+    return bson_set_array(NULL, key, doc);
+}
+
+static inline int bson_measure_boolean(const char *key)
+{
+    return bson_set_boolean(NULL, key, 0);
+}
+
+static inline int bson_measure_int32(const char *key)
+{
+    return bson_set_int32(NULL, key, 0);
+}
+
+static inline int bson_measure_double(const char *key)
+{
+    return bson_set_double(NULL, key, 0.0);
+}
+
+static inline int bson_measure_null(const char *key)
+{
+    return bson_set_null(NULL, key);
+}
+
+static inline int bson_measure_binary(const char *key, int binlen)
+{
+    return bson_set_binary_generic(NULL, key, binlen, NULL);
+}
+
+static inline int bson_measure_element(const char *key, const void *doc, int offset)
+{
+    return bson_set_element(NULL, key, doc, offset);
+}
 
 extern void *bson_alloc(int content_length);
 extern void bson_free(void *doc);
--- peridot_rpc_server/HAL/src/bson.c
+++ peridot_rpc_server/HAL/src/bson.c
@@ -329,11 +329,6 @@ int bson_set_string(void *doc, const char *key, const char *string)
 	return 1 + keylen + 4 + datalen;
 }
 
-int bson_measure_string(const char *key, const char *string)
-{
-	return bson_set_string(NULL, key, string);
-}
-
 static int set_subelement(void *doc, const char *key, char type, const void *ptr, int len)
 {
 	int keylen = strlen(key) + 1;
@@ -366,19 +361,9 @@ int bson_set_subdocument(void *doc, const char *key, const void *sub_doc)
 	return set_subdocument(doc, key, 0x03, sub_doc);
 }
 
-int bson_measure_subdocument(const char *key, const void *sub_doc)
-{
-	return set_subdocument(NULL, key, 0x03, sub_doc);
-}
-
 int bson_set_array(void *doc, const char *key, const void *sub_doc)
 {
 	return set_subdocument(doc, key, 0x04, sub_doc);
-}
-
-int bson_measure_array(const char *key, const void *sub_doc)
-{
-	return set_subdocument(NULL, key, 0x04, sub_doc);
 }
 
 int bson_set_boolean(void *doc, const char *key, int value)
@@ -387,19 +372,9 @@ int bson_set_boolean(void *doc, const char *key, int value)
 	return set_subelement(doc, key, 0x08, &bool_value, 1);
 }
 
-int bson_measure_boolean(const char *key)
-{
-	return bson_set_boolean(NULL, key, 0);
-}
-
 int bson_set_int32(void *doc, const char *key, int value)
 {
 	return set_subelement(doc, key, 0x10, &value, 4);
-}
-
-int bson_measure_int32(const char *key)
-{
-	return bson_set_int32(NULL, key, 0);
 }
 
 int bson_set_double(void *doc, const char *key, double value)
@@ -407,19 +382,9 @@ int bson_set_double(void *doc, const char *key, double value)
 	return set_subelement(doc, key, 0x01, &value, 8);
 }
 
-int bson_measure_double(const char *key)
-{
-	return bson_set_double(NULL, key, 0.0);
-}
-
 int bson_set_null(void *doc, const char *key)
 {
 	return set_subelement(doc, key, 0x0a, NULL, 0);
-}
-
-int bson_measure_null(const char *key)
-{
-	return bson_set_null(NULL, key);
 }
 
 static int bson_set_binary(void *doc, const char *key, int binlen, void **bufptr, char subtype)
@@ -459,11 +424,6 @@ static int bson_set_binary(void *doc, const char *key, int binlen, void **bufptr
 int bson_set_binary_generic(void *doc, const char* key, int binlen, void **bufptr)
 {
 	return bson_set_binary(doc, key, binlen, bufptr, 0x00);
-}
-
-int bson_measure_binary(const char *key, int binlen)
-{
-	return bson_set_binary_generic(NULL, key, binlen, NULL);
 }
 
 int bson_shrink_binary(void *doc, void *buf, int binlen)
@@ -529,11 +489,6 @@ int bson_set_element(void *doc1, const char *key, const void *doc2, int offset)
 no_bson:
 not_valid_bson:
 	return 0;
-}
-
-int bson_measure_element(const char *key, const void *doc, int offset)
-{
-	return bson_set_element(NULL, key, doc, offset);
 }
 
 int bson_measure_document(const void *doc)
